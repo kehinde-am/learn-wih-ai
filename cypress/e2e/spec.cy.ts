07961aed547a142025491518929c87f0034ea66f
kehinde-am <kennyamusa99@gmail.com>
2024-09-26 08:22:11 +0100

    test: e2e test passed

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,5 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('The Home Page', () => {
+  it('successfully loads', () => {
+    cy.visit('http://localhost:3000');
+  });
+});
